6ff3863fc28c5154c64b99a1dcf3f2738410313b
ogh96420 <ogh96420@naver.com>
2022-01-26 10:15:16 +0900

    애드몹 에러까지 잡음. 근데 광고배너 안나옴 (애드몹 코드 내가 생성했음 / 테스트코드 x)

--- app/src/main/java/com/example/plan_a/MainActivity.java
+++ app/src/main/java/com/example/plan_a/MainActivity.java
@@ -7,15 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends FragmentActivity {
     MainFragment fragment_main;
     MenuFragment fragment_menu;
     Button button_setting;
+    private AdView mAdview; //애드뷰 변수 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() { //광고 초기화
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdview = findViewById(R.id.adView); //배너광고 레이아웃 가져오기
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdview.loadAd(adRequest);
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER); //광고 사이즈는 배너 사이즈로 설정
+        adView.setAdUnitId("\n" + "ca-app-pub-8281537151369593/2640765776");
 
         /*fragment_main = new MainFragment();
         fragment_menu = new MenuFragment();
